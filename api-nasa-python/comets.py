48e97676f8c210f4daa01428590240ed02ff20b1
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-04-15 11:26:06 -0500

    update

--- api-nasa-python/comets.py
+++ api-nasa-python/comets.py
@@ -12,15 +12,20 @@ def get_commet_data(api_key):
         
         #Show:
         #Comet name
-        # os.system('clear')
+        os.system('clear')
         #Absolute magnide
         print(f"Comet name: {data['name']} ")
         #Estimated diameter max(KM)
+        print(f"Comet Estimated diameter max(KM): {data['estimated_diameter']['kilometers']['estimated_diameter_max']} ")
         #Estimated diameter min(KM)
+        print(f"Comet Estimated diameter min(KM): {data['estimated_diameter']['kilometers']['estimated_diameter_min']} ")
         #Estimated diameter max(FT)
+        print(f"Comet Estimated diameter max(FT): {data['estimated_diameter']['feet']['estimated_diameter_max']} ")
         #Estimated diameter min(FT)
+        print(f"Comet Estimated diameter min(FT): {data['estimated_diameter']['feet']['estimated_diameter_min']} ")
         #orbutal_data:
             #last_observation_date
+        print(f"last_observation_date: {data['orbital_data']['last_observation_date']} ")
     except requests.exceptions.RequestException as e:
         print(f"API error: {e}")
 #Main
